8c91b9fe18f787732ee8d8cc47bb18063374d170
devsong26 <devsong26@gmail.com>
2022-08-02 16:10:59 +0900

    8/2 공부 - 3

---
+++ study/src/chapter1/SafeListener.java
@@ -0,0 +1,26 @@
+package chapter1;
+
+/**
+ * 생성 메서드에서 this 변수가 유출되지 않도록 팩토리 메서드를 사용하는 모습
+ */
+public class SafeListener {
+    private final EventListener listener;
+
+    private SafeListener(){
+        listener = new EventListener() {
+            public void onEvent(Event e){
+                doSomething(e);
+            }
+        };
+    }
+
+    private void doSomething(Event e) {
+    }
+
+    public static SafeListener newInstance(EventSource source){
+        SafeListener safe = new SafeListener();
+        source.registerListener(safe.listener);
+        return safe;
+    }
+
+}
