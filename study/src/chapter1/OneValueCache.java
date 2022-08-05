f96dd091f3e577c92d46b4f2ed3557d943de4ad1
devsong26 <devsong26@gmail.com>
2022-08-05 21:01:30 +0900

    8/5 공부 - 1

---
+++ study/src/chapter1/OneValueCache.java
@@ -0,0 +1,30 @@
+package chapter1;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+/**
+ * 입력 값과 인수분해된 결과를 묶는 불변 객체
+ */
+@Immutable
+public class OneValueCache {
+
+    private final BigInteger lastNumber;
+    private final BigInteger[] lastFactors;
+
+    public OneValueCache(BigInteger lastNumber, BigInteger[] lastFactors) {
+        this.lastNumber = lastNumber;
+        this.lastFactors = lastFactors;
+    }
+
+    public BigInteger[] getFactors(BigInteger i){
+        if(lastNumber == null || !lastNumber.equals(i)){
+            return null;
+        }else{
+            return Arrays.copyOf(lastFactors, lastFactors.length);
+        }
+    }
+
+}
---
+++ study/src/chapter1/VolatileCachedFactorizer.java
@@ -0,0 +1,48 @@
+package chapter1;
+
+import annotation.ThreadSafe;
+
+import java.math.BigInteger;
+
+/**
+ *  최신 값을 불변 객체에 넣어 volatile 변수에 보관
+ */
+@ThreadSafe 
+public class VolatileCachedFactorizer implements Servlet {
+
+    private volatile OneValueCache cache = new OneValueCache(null, null);
+
+    public void service(ServletRequest req, ServletResponse resp){
+        BigInteger i = extractFromRequest(req);
+        BigInteger[] factors = cache.getFactors(i);
+
+        if(factors == null){
+            factors = factor(i);
+            cache = new OneValueCache(i, factors);
+        }
+
+        encodeIntoResponse(resp, factors);
+    }
+
+    private BigInteger extractFromRequest(ServletRequest req) {
+        return null;
+    }
+
+    private BigInteger[] factor(BigInteger i) {
+        return null;
+    }
+
+    private void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
+    }
+
+    /**
+     * 동기화 하지 않고 객체를 외부에 공개. 이런 코드는 금물!
+     * public 으로 내부 필드를 접근가능하도록 한 것
+     */
+    public Holder holder;
+
+    public void initialize(){
+        holder = new Holder(42);
+    }
+
+}
