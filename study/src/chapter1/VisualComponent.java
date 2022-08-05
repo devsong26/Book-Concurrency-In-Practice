a96353b27dbd65016c7381ce38327382c46291b8
devsong26 <devsong26@gmail.com>
2022-08-05 22:35:02 +0900

    8/5 공부 - 3

---
+++ study/src/chapter1/VisualComponent.java
@@ -0,0 +1,33 @@
+package chapter1;
+
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * 두 개 이상의 변수에게 스레드 안전성을 위임
+ */
+public class VisualComponent {
+    private final List<KeyListener> keyListeners
+            = new CopyOnWriteArrayList<>();
+    private final List<MouseListener> mouseListeners
+            = new CopyOnWriteArrayList<>();
+
+    public void addKeyListener(KeyListener listener){
+        keyListeners.add(listener);
+    }
+
+    public void addMouseListener(MouseListener listener){
+        mouseListeners.add(listener);
+    }
+
+    public void removeKeyListener(KeyListener listener){
+        keyListeners.remove(listener);
+    }
+
+    public void removeMouseListener(MouseListener listener){
+        mouseListeners.remove(listener);
+    }
+
+}
