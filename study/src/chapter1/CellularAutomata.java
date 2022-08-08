20692ea277fdc35a363691aa8ecf18827afc1fbb
devsong26 <devsong26@gmail.com>
2022-08-08 23:22:30 +0900

    8/8 공부 - 1

--- study/src/chapter1/CellularAutomata.java
+++ study/src/chapter1/CellularAutomata.java
@@ -145,4 +145,41 @@ public class CellularAutomata {
         }
     }
 
+    /**
+     * Memoizer 최종 버전
+     */
+    public class Memoizer<A, V> implements Computable<A, V> {
+        private final ConcurrentMap<A, Future<V>> cache
+                = new ConcurrentHashMap<>();
+        private final Computable<A, V> c;
+
+        public Memoizer(Computable<A, V> c){
+            this.c = c;
+        }
+
+        public V compute(final A arg) throws InterruptedException{
+            while(true){
+                Future<V> f = cache.get(arg);
+                if(f == null){
+                    Callable<V> eval = () -> c.compute(arg);
+
+                    FutureTask<V> ft = new FutureTask<>(eval);
+                    f = cache.putIfAbsent(arg, ft);
+                    if (f == null){
+                        f = ft;
+                        ft.run();
+                    }
+
+                    try{
+                        return f.get();
+                    }catch(CancellationException e){
+                        cache.remove(arg, f);
+                    }catch(ExecutionException e){
+                        throw launderThrowable(e.getCause());
+                    }
+                }
+            }
+        }
+    }
+
 }
