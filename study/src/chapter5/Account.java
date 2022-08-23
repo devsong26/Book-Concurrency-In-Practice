1f5bb59491074fe0d029f493b65bd439a7faf6f6
SONGINGWON <devsong26@gmail.com>
2022-08-23 21:14:19 +0900

    8/23 공부 - 2

---
+++ study/src/chapter5/Account.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+public class Account {
+    public Comparable<DollarAmount> getBalance() {
+        return null;
+    }
+
+    public void debit(DollarAmount amount) {
+    }
+
+    public void credit(DollarAmount amount) {
+    }
+}
---
+++ study/src/chapter5/InsufficientFundsException.java
@@ -0,0 +1,4 @@
+package chapter5;
+
+public class InsufficientFundsException extends Exception {
+}
