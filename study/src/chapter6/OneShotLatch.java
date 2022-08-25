1f9992ca368f7d93581c2d54b2718f92ab79d36e
SONGINGWON <devsong26@gmail.com>
2022-08-25 18:13:56 +0900

    8/25 공부 - 2

--- study/src/chapter6/OneShotLatch.java
+++ study/src/chapter6/OneShotLatch.java
@@ -20,14 +20,25 @@ public class OneShotLatch {
     }
 
     private class Sync extends AbstractQueuedSynchronizer {
-        protected int tryAcquireShared(int ignored){
-            // 래치가 열려 있는 상태(state == 1) 라면 성공, 아니면 실패
-            return (getState() == 1) ? 1 : -1;
+        /**
+         * Semaphore 클래스의 tryAcquireShared 메서드와 tryReleaseShared 메서드
+         */
+        protected int tryAcquireShared(int acquires){
+            while(true){
+                int available = getState();
+                int remaining = available - acquires;
+                if(remaining < 0
+                        || compareAndSetState(available, remaining))
+                    return remaining;
+            }
         }
 
-        protected boolean tryReleaseShared(int ignored){
-            setState(1);    // 래치가 열렸다.
-            return true;    // 다른 스레드에서 확보 연산에 성공할 가능성이 있다
+        protected boolean tryReleaseShared(int release){
+            while(true){
+                int p = getState();
+                if (compareAndSetState(p, p + release))
+                    return true;
+            }
         }
 
         /**
