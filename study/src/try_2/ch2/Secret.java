b93a85e5f3e67d9b0fa64b22422e52da6aeacf35
SONGINGWON <devsong26@gmail.com>
2022-08-31 21:03:19 +0900

    객체 공개

---
+++ study/src/try_2/ch2/Secret.java
@@ -0,0 +1,4 @@
+package try_2.ch2;
+
+public class Secret {
+}
--- study/src/try_2/ch2/UnsafeStates.java
+++ study/src/try_2/ch2/UnsafeStates.java
@@ -1,5 +1,8 @@
 package try_2.ch2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 내부적으로 사용할 변수를 외부에 공개, 이런 코드는 금물!
  */
@@ -10,4 +13,14 @@ public class UnsafeStates {
     public String[] getStates() {
         return states;
     }
+
+    /**
+     * 객체 공개
+     */
+    public static Set<Secret> knownSecrets;
+
+    public void initialize(){
+        knownSecrets = new HashSet<>();
+    }
+
 }
