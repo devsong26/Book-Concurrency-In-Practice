ef8105941723b8b87498da37b09e82fdc5584a25
SONGINGWON <devsong26@gmail.com>
2022-09-02 20:13:15 +0900

    동기화 하지 않고 객체를 외부에 공개, 이런 코드는 금물!

---
+++ study/src/try_2/ch2/Holder.java
@@ -0,0 +1,6 @@
+package try_2.ch2;
+
+public class Holder {
+    public Holder(int i) {
+    }
+}
--- study/src/try_2/ch2/VolatileCachedFactorizer.java
+++ study/src/try_2/ch2/VolatileCachedFactorizer.java
@@ -38,4 +38,15 @@ public class VolatileCachedFactorizer implements Servlet {
     private void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
 
     }
+
+    /**
+     * 동기화 하지 않고 객체를 외부에 공개, 이런 코드는 금물!
+     */
+    // 안전하지 않은 객체 공개
+    public Holder holder;
+
+    public void initialize(){
+        holder = new Holder(42);
+    }
+
 }
