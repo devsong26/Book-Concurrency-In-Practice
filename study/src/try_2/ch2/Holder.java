ea8f80a5dfcfdef0ca30dcc7f6bebe44c1aa3c94
SONGINGWON <devsong26@gmail.com>
2022-09-02 20:15:31 +0900

    올바르게 공개하지 않으면 문제가 생길 수 있는 객체

--- study/src/try_2/ch2/Holder.java
+++ study/src/try_2/ch2/Holder.java
@@ -1,6 +1,18 @@
 package try_2.ch2;
 
+/**
+ * 올바르게 공개하지 않으면 문제가 생길 수 있는 객체
+ */
 public class Holder {
-    public Holder(int i) {
+    private final int n;
+
+    public Holder(int n) {
+        this.n = n;
     }
+
+    public void assertSanity(){
+        if(n != n)
+            throw new AssertionError("This statement is false.");
+    }
+
 }
