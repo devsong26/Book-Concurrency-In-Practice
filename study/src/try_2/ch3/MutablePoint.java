da9fc425795513f62cae45cf3d1e324732e746be
SONGINGWON <devsong26@gmail.com>
2022-09-03 11:12:08 +0900

    java.awt.Point와 유사하지만 변경 가능한 MutablePoint 클래스

---
+++ study/src/try_2/ch3/MutablePoint.java
@@ -0,0 +1,21 @@
+package try_2.ch3;
+
+import annotation.NotThreadSafe;
+
+/**
+ * java.awt.Point와 유사하지만 변경 가능한 MutablePoint 클래스
+ */
+@NotThreadSafe
+public class MutablePoint {
+    public int x, y;
+
+    public MutablePoint(){
+        x = 0;
+        y = 0;
+    }
+
+    public MutablePoint(MutablePoint p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+}
