53ad281d6769b211abe8815c23badff12c9a4675
SONGINGWON <devsong26@gmail.com>
2022-09-04 14:27:15 +0900

    기존의 Vector 클래스를 상속받아 putIfAbsent 메서드를 추가

---
+++ study/src/try_2/ch3/BetterVector.java
@@ -0,0 +1,21 @@
+package try_2.ch3;
+
+import annotation.ThreadSafe;
+
+import java.util.Vector;
+
+/**
+ * 기존의 Vector 클래스를 상속받아 putIfAbsent 메서드를 추가
+ */
+@ThreadSafe
+public class BetterVector<E> extends Vector<E> {
+    public synchronized boolean putIfAbsent(E x){
+        boolean absent = !contains(x);
+
+        if (absent){
+            add(x);
+        }
+
+        return absent;
+    }
+}
