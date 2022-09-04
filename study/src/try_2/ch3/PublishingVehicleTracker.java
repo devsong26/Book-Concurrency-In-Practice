1b9b4d6ff93540e0a1be18a31d0c449b0aed4b78
SONGINGWON <devsong26@gmail.com>
2022-09-04 14:22:58 +0900

    내부 상태를 안전하게 공개하는 차량 추적 프로그램

---
+++ study/src/try_2/ch3/PublishingVehicleTracker.java
@@ -0,0 +1,39 @@
+package try_2.ch3;
+
+import annotation.ThreadSafe;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 내부 상태를 안전하게 공개하는 차량 추적 프로그램
+ */
+@ThreadSafe
+public class PublishingVehicleTracker {
+    private final Map<String, SafePoint> locations;
+    private final Map<String, SafePoint> unmodifiableMap;
+
+    public PublishingVehicleTracker(
+            Map<String, SafePoint> locations){
+        this.locations = new ConcurrentHashMap<>(locations);
+        this.unmodifiableMap = Collections.unmodifiableMap(this.locations);
+    }
+
+    public Map<String, SafePoint> getLocations(){
+        return unmodifiableMap;
+    }
+
+    public SafePoint getLocations(String id){
+        return locations.get(id);
+    }
+
+    public void setLocation(String id, int x, int y){
+        if (!locations.containsKey(id)){
+            throw new IllegalArgumentException("invalid vehicle name: " + id);
+        }
+
+        locations.get(id).set(x, y);
+    }
+
+}
