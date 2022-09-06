4cf904e44b3674754b2bd210c1d136db69aa4080
SONGINGWON <devsong26@gmail.com>
2022-09-06 22:02:39 +0900

    FutureTask를 사용해 추후 필요한 데이터를 미리 읽어들이는 모습 - 1

---
+++ study/src/try_2/ch4/PreLoader.java
@@ -0,0 +1,46 @@
+package try_2.ch4;
+
+import try_1.chapter1.DataLoadException;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * FutureTask를 사용해 추후 필요한 데이터를 미리 읽어들이는 모습
+ */
+public class PreLoader {
+    private final FutureTask<ProductInfo> future =
+            new FutureTask<>(new Callable<>(){
+               public ProductInfo call() throws DataLoadException{
+                   return loadProductInfo();
+               }
+
+                private ProductInfo loadProductInfo() {
+                   return null;
+                }
+            });
+
+    private final Thread thread = new Thread(future);
+
+    public void start(){
+        thread.start();
+    }
+
+    public ProductInfo get()
+        throws DataLoadException, InterruptedException{
+        try{
+            return future.get();
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            if(cause instanceof DataLoadException){
+                throw (DataLoadException) cause;
+            }else{
+                throw launderThrowable(cause);            }
+        }
+        }
+
+    private DataLoadException launderThrowable(Throwable cause) {
+        return null;
+    }
+}
