5d6f60c1437170d397dfe96b9883653aee3e7c9e
SONGINGWON <devsong26@gmail.com>
2022-09-06 21:52:58 +0900

    인터럽트가 발생했음을 저장해 인터럽트 상황을 잊지 않도록 한다.

---
+++ study/src/try_2/ch4/TaskRunnable.java
@@ -0,0 +1,25 @@
+package try_2.ch4;
+
+import try_1.chapter1.Task;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * 인터럽트가 발생했음을 저장해 인터럽트 상황을 잊지 않도록 한다.
+ */
+public class TaskRunnable implements Runnable{
+    BlockingQueue<Task> queue;
+
+    public void run(){
+        try{
+            processTask(queue.take());
+        } catch (InterruptedException e) {
+            // 인터럽트가 발생한 사실을 저장한다.
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void processTask(Task take) {
+    }
+
+}
