2cf485efb725dad291f369689c0411af1e938de0
SONGINGWON <devsong26@gmail.com>
2022-09-07 21:36:57 +0900

    HashMap과 동기화 기능을 사용해 구현한 첫 번째 캐시

---
+++ study/src/try_2/ch4/Sample.java
@@ -0,0 +1,43 @@
+package try_2.ch4;
+
+import annotation.GuardedBy;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * HashMap과 동기화 기능을 사용해 구현한 첫 번째 캐시
+ */
+public class Sample {
+    public interface Computable<A, V>{
+        V compute(A arg) throws InterruptedException;
+    }
+
+    public class ExpensiveFunction implements Computable<String, BigInteger>{
+        public BigInteger compute(String arg){
+            // 잠시 생각 좀 하고...
+            return new BigInteger(arg);
+        }
+    }
+
+    public class Memoizer1<A, V> implements Computable<A, V>{
+        @GuardedBy("this")
+        private final Map<A, V> cache = new HashMap<>();
+        private final Computable<A, V> c;
+
+        public Memoizer1(Computable<A, V> c){
+            this.c = c;
+        }
+
+        public synchronized V compute(A arg) throws InterruptedException{
+            V result = cache.get(arg);
+            if (result == null){
+                result = c.compute(arg);
+                cache.put(arg, result);
+            }
+            return result;
+        }
+    }
+
+}
