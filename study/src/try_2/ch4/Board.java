fd735837059fad3f1fe7d07756ccc6e7fc4e17ea
SONGINGWON <devsong26@gmail.com>
2022-09-07 20:47:24 +0900

    CyclicBarrier를 사용해 셀룰러 오토마타의 연산을 제어

---
+++ study/src/try_2/ch4/Board.java
@@ -0,0 +1,28 @@
+package try_2.ch4;
+
+public class Board {
+    public void commitNewValues() {
+    }
+
+    public Board getSubBoard(int count, int i) {
+        return null;
+    }
+
+    public boolean hasConverged() {
+        return false;
+    }
+
+    public int getMaxX() {
+        return 0;
+    }
+
+    public int getMaxY() {
+        return 0;
+    }
+
+    public void setNewValue(int x, int y, Object computeValue) {
+    }
+
+    public void waitForConvergence() {
+    }
+}
