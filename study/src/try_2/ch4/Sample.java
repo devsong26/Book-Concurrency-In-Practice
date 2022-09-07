3e91da5e0bb26489b97438d2e031fc070b4fc21e
SONGINGWON <devsong26@gmail.com>
2022-09-07 21:40:20 +0900

    HashMap 대신 ConcurrentHashMap을 적용

--- study/src/try_2/ch4/Sample.java
+++ study/src/try_2/ch4/Sample.java
@@ -5,6 +5,7 @@ import annotation.GuardedBy;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * HashMap과 동기화 기능을 사용해 구현한 첫 번째 캐시
@@ -33,6 +34,27 @@ public class Sample {
         public synchronized V compute(A arg) throws InterruptedException{
             V result = cache.get(arg);
             if (result == null){
+                result = c.compute(arg);
+                cache.put(arg, result);
+            }
+            return result;
+        }
+    }
+
+    /**
+     * HashMap 대신 ConcurrentHashMap을 적용
+     */
+    public class Memoizer2<A, V> implements Computable<A, V> {
+        private final Map<A, V> cache = new ConcurrentHashMap<A, V>();
+        private final Computable<A, V> c;
+
+        public Memoizer2(Computable<A, V> c){
+            this.c = c;
+        }
+
+        public V compute(A arg) throws InterruptedException{
+            V result = cache.get(arg);
+            if(result == null){
                 result = c.compute(arg);
                 cache.put(arg, result);
             }
