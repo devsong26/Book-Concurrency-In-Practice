57524b91a34cca93082668deee9a0fd9670bbea6
SONGINGWON <devsong26@gmail.com>
2022-09-24 11:54:04 +0900

    메서드 내부에서 Executor를 사용하는 모습

--- study/src/try_2/ch6/IndexingService.java
+++ study/src/try_2/ch6/IndexingService.java
@@ -2,7 +2,12 @@ package try_2.ch6;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * 독약 객체를 사용해 서비스를 종료
@@ -94,6 +99,34 @@ public class IndexingService {
 
     public void awaitTermination() throws InterruptedException {
         consumer.join();
+    }
+
+    /**
+     * 메서드 내부에서 Executor를 사용하는 모습
+     */
+    boolean checkMail(Set<String> hosts, long timeout, TimeUnit unit)
+        throws InterruptedException {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final AtomicBoolean hasNewMail = new AtomicBoolean(false);
+
+        try {
+            for (final String host : hosts)
+                exec.execute(() -> {
+                    if(checkMail(host))
+                        hasNewMail.set(true);
+                });
+        } finally {
+            exec.shutdown();
+            exec.awaitTermination(timeout, unit);
+        }
+
+        return hasNewMail.get();
+    }
+
+    private boolean checkMail(String host) {
+        System.out.println(host);
+        return true;
     }
 
 }
